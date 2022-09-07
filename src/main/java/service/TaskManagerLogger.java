2633669bd1d3533dd2145fc06d29fb307115f5b5
e5k4p3 <e5k4p3@gmail.com>
2022-09-07 11:14:14 +0300

    add basic logger implementation

---
+++ src/main/java/service/TaskManagerLogger.java
@@ -0,0 +1,46 @@
+package service;
+
+import service.interfaces.BasicLogger;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManagerLogger implements BasicLogger {
+    private final File file;
+
+    public TaskManagerLogger(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void addMessageToLog(String message) {
+        try (Writer fileWriter = new FileWriter(file, true)) {
+            fileWriter.write(LocalDateTime.now() + " " + message);
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время добавления сообщения в log файл");
+        }
+    }
+
+    @Override
+    public void clearLogFile() {
+        try (Writer fileWriter = new FileWriter(file)) {
+            fileWriter.write("");
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время очистки log файла");
+        }
+    }
+
+    @Override
+    public List<String> getLoggedMessages() {
+        try {
+            return Files.readAllLines(Paths.get(String.valueOf(file)));
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка во время считывания log файла");
+        }
+        return new ArrayList<>();
+    }
+}
